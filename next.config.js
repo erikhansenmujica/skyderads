ddb1e0af2f07ffe4d3fcf1c95d04b9eaefe69d4c
Erik Hansen <erikhansenmujica@gmail.com>
2023-11-01 13:51:37 +0000

    redirect 308

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        // What the user typed in the browser
+        source: "https://skyderads.com",
+        // Where the user will be redirected to
+        destination: "https://skyderdigital.com",
+        // If the destination is a permanent redirect (308)
+        permanent: true,
+      },
+    ];
+  },
+};
 
 module.exports = nextConfig;
