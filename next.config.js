b35af76affaa7d9db0a37aa50058a4fadb8d35e1
Erik Hansen <erikhansenmujica@gmail.com>
2023-11-01 14:30:06 +0000

    update

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  async redirects() {
-    return [
-      {
-        // What the user typed in the browser
-        source: "https://skyderads.com",
-        // Where the user will be redirected to
-        destination: "https://skyderdigital.com",
-        // If the destination is a permanent redirect (308)
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
