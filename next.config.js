f3b2107c113d8fe669d973f4397239c2358d7a55
Erik Hansen <erikhansenmujica@gmail.com>
2023-11-16 11:16:58 +0000

    redirections

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => [
+    {
+      source: "/:path*",
+      has: [{ type: "host", value: "www.skyderdigital.com" }],
+      destination: "https://skyderdigital.com/:path*",
+      permanent: true,
+    },
+  ],
+};
 
 module.exports = nextConfig;
